7c0ab148cd5052cb509b22492e48bb470acf57ce
sacoDePapas69 <101902747+sacoDePapas69@users.noreply.github.com>
2022-07-15 21:55:12 -0500

    Update settings.py

--- mercadoverde/mercadoverde/mercadoverde/settings.py
+++ mercadoverde/mercadoverde/mercadoverde/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'mercadoverde.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'prueba3',
+        'USER': 'admin',
+        'PASSWORD': '1234',        
+        'HOST':'127.0.0.1',
+        'PORT':'1521'
     }
 }
 
